79b86bbd4383d0fc8c310fa11f3ac8f004f22645
İlker Avcı <ikolomiko@gmail.com>
2024-07-01 18:46:08 +0300

    Remove all GUI elements and get apk path from argv

--- BadUnboxing/src/main/java/com/lauriewired/BadUnboxing.java
+++ BadUnboxing/src/main/java/com/lauriewired/BadUnboxing.java
@@ -1,177 +1,88 @@
 package com.lauriewired;
 
-import java.awt.datatransfer.DataFlavor;
-import java.awt.dnd.DnDConstants;
-import java.awt.dnd.DropTarget;
-import java.awt.dnd.DropTargetAdapter;
-import java.awt.dnd.DropTargetDropEvent;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.io.File;
-import java.net.URL;
-import java.util.List;
+import com.lauriewired.analyzer.ApkAnalysisDetails;
+import com.lauriewired.analyzer.DynamicDexLoaderDetection;
+import com.lauriewired.analyzer.JadxUtils;
+import com.lauriewired.analyzer.UnpackerGenerator;
 
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.weisj.darklaf.LafManager;
-import com.github.weisj.darklaf.theme.DarculaTheme;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
-import com.lauriewired.ui.AnalysisWindow;
+import jadx.api.JadxDecompiler;
 
 public class BadUnboxing {
     private static final Logger logger = LoggerFactory.getLogger(BadUnboxing.class);
 
     public static void main(String[] args) {
-        // Set the Darklaf Look and Feel
-        try {
-            LafManager.install(new DarculaTheme());
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (args.length != 1) {
+            System.err.println("usage: java -jar BadUnboxing.jar /path/to/apk");
+            System.exit(1);
         }
-
-        SwingUtilities.invokeLater(BadUnboxing::createAndShowGUI);
+        String apkFilePath = args[0];
+        analyzeApk(apkFilePath);
     }
 
-    private static void createAndShowGUI() {
-        JFrame frame = new JFrame("BadUnboxing");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400); // Increased size for better spacing
-    
-        // Set the window icon
+    private static List<String> isPacked(String apkFilePath, JadxDecompiler jadx) {
+        List<String> packedClasses = new ArrayList<>();
         try {
-            URL imageUrl = BadUnboxing.class.getClassLoader().getResource("icon.png");
-            if (imageUrl != null) {
-                ImageIcon imageIcon = new ImageIcon(imageUrl);
-                frame.setIconImage(imageIcon.getImage());
-            } else {
-                logger.error("Icon resource not found");
-            }
-        } catch (Exception e) {
-            logger.error("Failed to load window icon", e);
-        }
-    
-        JPanel panel = new JPanel();
-        frame.add(panel);
-        placeComponents(panel, frame);
-    
-        frame.setVisible(true);
-    }
-    
-    private static void placeComponents(JPanel panel, JFrame frame) {
-        panel.setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.insets = new Insets(15, 15, 15, 15);  // Larger margins around components
-    
-        /*
-        // Label "APK File:"
-        JLabel label = new JLabel("APK Unpacker");
-        label.setFont(new Font("Verdana", Font.BOLD, 25));  // Set font style and size
-        constraints.gridx = 0;  // Column 0
-        constraints.gridy = 0;  // Row 0
-        constraints.gridwidth = 4;  // Span across all columns
-        constraints.anchor = GridBagConstraints.CENTER;  // Center alignment
-        constraints.insets = new Insets(20, 15, 20, 15); // Add padding
-        panel.add(label, constraints);
-        */
-    
-        // Text Field for file path
-        JTextField filePathText = new JTextField();
-        filePathText.setFont(new Font("Verdana", Font.PLAIN, 16)); // Set font style and size
-        filePathText.setEditable(false);
-        filePathText.setPreferredSize(new Dimension(275, 30));  // Set preferred size for wider text field
-        //filePathText.setBorder(BorderFactory.createEmptyBorder()); // Remove the white border
-        constraints.gridx = 0;  // Column 0
-        constraints.gridy = 1;  // Row 1
-        constraints.gridwidth = 3;  // Takes three columns
-        constraints.anchor = GridBagConstraints.CENTER;  // Center alignment
-        constraints.insets = new Insets(10, 15, 10, 5); // Add padding
-        panel.add(filePathText, constraints);
-    
-        // "Select File" button
-        JButton fileButton = new JButton("Select File");
-        constraints.gridx = 3;  // Column 3
-        constraints.gridy = 1;  // Row 1
-        constraints.gridwidth = 1;  // Takes one column
-        constraints.anchor = GridBagConstraints.CENTER;  // Center alignment
-        constraints.insets = new Insets(5, 5, 5, 15); // Add padding
-        panel.add(fileButton, constraints);
-    
-        // Status label
-        JLabel statusLabel = new JLabel("");
-        statusLabel.setFont(new Font("Verdana", Font.ITALIC, 14));  // Set font style and size
-        constraints.gridx = 0;  // Column 0
-        constraints.gridy = 3;  // Row 3
-        constraints.gridwidth = 4;  // Span across all columns
-        constraints.anchor = GridBagConstraints.CENTER;  // Center alignment
-        constraints.insets = new Insets(10, 15, 10, 15); // Add padding
-        panel.add(statusLabel, constraints);
-    
-        // "Unpack" button
-        JButton unpackButton = new JButton("Generate Unpacker");
-        constraints.gridx = 0;  // Column 0
-        constraints.gridy = 2;  // Row 2
-        constraints.gridwidth = 4;  // Span across all columns
-        constraints.anchor = GridBagConstraints.CENTER;  // Center alignment
-        constraints.insets = new Insets(10, 15, 10, 15); // Add padding
-        panel.add(unpackButton, constraints);
-    
-        // File and button listeners
-        setupDragAndDrop(filePathText);
-        setupFileButtonListener(fileButton, filePathText, panel);
-        setupUnpackButtonListener(unpackButton, filePathText, statusLabel, frame);
-    }
-    
-    private static void setupDragAndDrop(JTextField filePathText) {
-        new DropTarget(filePathText, new DropTargetAdapter() {
-            @Override
-            public void drop(DropTargetDropEvent evt) {
-                try {
-                    evt.acceptDrop(DnDConstants.ACTION_COPY);
-                    List<File> droppedFiles = (List<File>) evt.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
-                    if (!droppedFiles.isEmpty()) {
-                        File file = droppedFiles.get(0);
-                        filePathText.setText(file.getAbsolutePath());
-                    }
-                } catch (Exception ex) {
-                    logger.error("Error during file drop", ex);
+            // Extract and parse AndroidManifest.xml
+            Set<String> manifestClasses = JadxUtils.getManifestClasses(apkFilePath, jadx);
+
+            // Get classes from dex files
+            Set<String> dexClasses = JadxUtils.getDexClasses(apkFilePath, jadx);
+
+            // Check if there are any classes in the manifest that are not in the dex files
+            for (String className : manifestClasses) {
+                if (!dexClasses.contains(className)) {
+                    logger.info("Class {} found in manifest but not in dex files", className);
+                    packedClasses.add(className);
                 }
             }
-        });
-    }
-    
-    private static void setupFileButtonListener(JButton fileButton, JTextField filePathText, JPanel panel) {
-        fileButton.addActionListener(e -> {
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-            int option = fileChooser.showOpenDialog(panel);
-            if (option == JFileChooser.APPROVE_OPTION) {
-                File selectedFile = fileChooser.getSelectedFile();
-                filePathText.setText(selectedFile.getAbsolutePath());
-            }
-        });
+        } catch (Exception e) {
+            logger.error("Error checking if APK is packed", e);
+        }
+        return packedClasses; // Return the list of packed classes
     }
 
-    private static void setupUnpackButtonListener(JButton unpackButton, JTextField filePathText, JLabel statusLabel, JFrame frame) {
-        unpackButton.addActionListener(e -> {
-            String apkFilePath = filePathText.getText();
-            if (!apkFilePath.isEmpty()) {
-                AnalysisWindow.show(frame, apkFilePath);  // Open Analysis Window
-            } else {
-                statusLabel.setText("Please enter a valid APK file path.");
-            }
-        });
+    public static void analyzeApk(String apkFilePath) {
+        logger.info("Loading APK");
+        JadxDecompiler jadx = JadxUtils.loadJadx(apkFilePath);
+
+        List<String> packedClasses = isPacked(apkFilePath, jadx);
+        if (packedClasses.isEmpty()) {
+            logger.info("APK is not packed");
+            return;
+        }
+
+        logger.info("APK is packed");
+
+        // Missing classes which were found in AndroidManifest.xml
+        logger.info("[BEGIN_LIST] Missing classes");
+        for (String className : packedClasses) {
+            logger.info(className);
+        }
+        logger.info("[END_LIST] Missing classes");
+
+        List<String> dexLoadingDetails = DynamicDexLoaderDetection.getJavaDexLoadingDetails(jadx);
+        if (dexLoadingDetails.isEmpty()) {
+            logger.info("Packer Type:Native");
+            return;
+        }
+
+        logger.info("Packer Type:Java");
+        logger.info("[BEGIN_LIST] Code loader details");
+        for (String detail : dexLoadingDetails) {
+            logger.info(detail);
+        }
+        logger.info("[END_LIST] Code loader details");
+
+        ApkAnalysisDetails apkAnalysisDetails = UnpackerGenerator.generateJava(jadx, apkFilePath);
+        if (apkAnalysisDetails.getBaseDir() == null) {
+            logger.error("Error generating Java unpacker code.");
+        }
     }
 }
